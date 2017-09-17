dc2caacdde5231ca4e7afd70653350d61c943213
xiaowu.zhou <xiaowu.zhou@tongdun.cn>
2017-09-17 10:49:42 +0800

    能启动，controller mapping 不能找到

---
+++ taotao-manager/taotao-manager-service/src/main/java/com/taotao/service/impl/ItemServiceImpl.java
@@ -0,0 +1,17 @@
+package com.taotao.service.impl;
+
+import com.taotao.pojo.TbItem;
+import com.taotao.service.ItemService;
+import org.springframework.stereotype.Service;
+
+/**
+ * Created by xiaowu.zhou@tongdun.cn on 2017/9/16.
+ */
+
+@Service
+public class ItemServiceImpl implements ItemService {
+    @Override
+    public TbItem getItemById(long itemId) {
+        return null;
+    }
+}
